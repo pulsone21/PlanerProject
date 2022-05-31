cda4255048b3b3a9a38eb3f16dc20226c81d3100
Johannes Böhm <pulsone21@freenet.de>
2022-05-31 16:57:16 +0200

    utility restructure

--- Assets/_Project/Utilities/Extensions.cs
+++ Assets/_Project/Utilities/Extensions.cs
@@ -1,11 +1,21 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace Utilities
 {
+
     public static class Extensions
     {
+        public static List<T> ToList<T>(this T[] array)
+        {
+            List<T> newList = new List<T>();
+            foreach (T item in array)
+            {
+                newList.Add(item);
+            }
+            return newList;
+        }
+
+
         public static List<T> ToList<T>(this HashSet<T> hashset)
         {
             List<T> list = new List<T>();
@@ -25,7 +35,5 @@ namespace Utilities
             }
             return hashset;
         }
-
-
     }
 }

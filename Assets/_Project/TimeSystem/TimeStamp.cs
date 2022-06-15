b9e4c20893eed3ff757016acc8460b229c289c88
Johannes Böhm <pulsone21@freenet.de>
2022-06-15 12:44:59 +0200

    adding more consts to not have rnd numbers

--- Assets/_Project/TimeSystem/TimeStamp.cs
+++ Assets/_Project/TimeSystem/TimeStamp.cs
@@ -1,5 +1,5 @@
 using System;
-
+using UnityEngine;
 namespace TimeSystem
 {
     public class TimeStamp
@@ -11,7 +11,15 @@ namespace TimeSystem
         public int Year { get; protected set; }
         public Season Season { get; protected set; }
 
-        public static TimeStamp INITIAL_TIMESTAMP = new TimeStamp(0, 0, 1, 1, 2020);
+        public static TimeStamp INITIAL_TIMESTAMP = new TimeStamp(0, 0, 1, 1, 1950);
+        private const int MIN_PER_YEAR = 525600;
+        private const int _19Year_IN_MIN = YEAR_IN_MIN * 19;
+        public const int YEAR_IN_MIN = 525600;
+        public const int MONTH_IN_MIN = 43800;
+        public const int DAY_IN_MIN = 1440;
+        public const int HOUR_IN_MIN = 60;
+
+        public bool IsNight => Hour > 6 && Hour < 18;
 
         public TimeStamp(int _minute, int _hour, int _day, int _month, int _year, Season _season)
         {
@@ -39,10 +47,7 @@ namespace TimeSystem
         /// <returns></returns>
         public long InMinutes() => GetTimeStampInMinutes(this);
 
-        public override string ToString()
-        {
-            return $"{Hour:00}:{Minute:00} / {Day:00}.{Month:00}.{Year:0000}";
-        }
+        public override string ToString() => $"{Hour:00}:{Minute:00} / {Day:00}.{Month:00}.{Year:0000}";
 
         /// <summary>
         /// Returns a TimeStamp in Minutes
@@ -51,12 +56,27 @@ namespace TimeSystem
         /// <returns>TimeStamp in Minutes</returns>
         public static int GetTimeStampInMinutes(TimeStamp _timeStamp)
         {
-            int timeStampInMinutes = _timeStamp.Year * 525600;
-            timeStampInMinutes += _timeStamp.Month * 43800;
-            timeStampInMinutes += _timeStamp.Day * 1440;
-            timeStampInMinutes += _timeStamp.Hour * 60;
+            int timeStampInMinutes = _timeStamp.Year * YEAR_IN_MIN;
+            timeStampInMinutes += _timeStamp.Month * MONTH_IN_MIN;
+            timeStampInMinutes += _timeStamp.Day * DAY_IN_MIN;
+            timeStampInMinutes += _timeStamp.Hour * HOUR_IN_MIN;
             timeStampInMinutes += _timeStamp.Minute;
             return timeStampInMinutes;
+        }
+
+        public int DifferenceToNowInYears()
+        {
+            long minutes = InMinutes() - TimeManager.Instance.CurrentTimeStamp.InMinutes();
+            Debug.Log($"Minutes: {minutes} - In Years: {Mathf.FloorToInt(minutes / MIN_PER_YEAR)}");
+            return Mathf.Abs(Mathf.FloorToInt(minutes / MIN_PER_YEAR));
+        }
+
+        public static TimeStamp GetRndBirthday(int maxAge = 65, long minAge = 18)
+        {
+            int rndMinute = Mathf.FloorToInt(UnityEngine.Random.Range(minAge * MIN_PER_YEAR, (maxAge + 1) * MIN_PER_YEAR));
+            long normalizedMinutes = TimeManager.Instance.CurrentTimeStamp.InMinutes() - TimeStamp.INITIAL_TIMESTAMP.InMinutes();
+            long birthDay = normalizedMinutes - (long)rndMinute;
+            return TimeStamp.GetTimeStampFromTotalMinutes(birthDay); ;
         }
 
         /// <summary>

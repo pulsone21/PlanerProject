f26a4e077b637f9f2f8dd54e05b2337cfc38ff8b
Johannes Böhm <pulsone21@freenet.de>
2022-06-16 14:08:14 +0200

    rearranging roadSystem

--- Assets/_Project/RoadSystem/RoadNetwork.cs
+++ Assets/_Project/RoadSystem/RoadNetwork.cs
@@ -11,7 +11,7 @@ namespace RoadSystem
 
         private void Awake()
         {
-            for (int i = 0; i < transform.childCount; i++) Roads.Add(transform.GetChild(i).GetComponent<Road>());
+            // for (int i = 0; i < transform.childCount; i++) Roads.Add(transform.GetChild(i).GetComponent<Road>());
         }
 
 

41ae562c0b1b4682a4e3b01ae447566c53660e41
Johannes Böhm <pulsone21@freenet.de>
2022-07-03 17:59:35 +0200

    refactor timeUI implementing canvas fader

--- Assets/_Project/UI/TimeUIController.cs
+++ Assets/_Project/UI/TimeUIController.cs
@@ -14,12 +14,11 @@ namespace UISystem
 
         [SerializeField, Range(0f, 1f)] private float ExpandDuration, ExpandDelay, ShrinkDuration, ShrinkDelay;
         private bool IsExpanded = false;
-        private const int EXPANDED_HIGHT = 95;
-        private const int SHRINKED_HEIGHT = 45;
+        [SerializeField] private int EXPANDED_HIGHT = 95;
+        [SerializeField] private int SHRINKED_HEIGHT = 45;
 
         public void ToogleExpand()
         {
-            Debug.Log("toogling Expand");
             IsExpanded = !IsExpanded;
             if (IsExpanded)
             {
@@ -35,7 +34,6 @@ namespace UISystem
         {
             if (!force) if (IsExpanded) return;
             IsExpanded = true;
-            ActionBar.gameObject.SetActive(true);
             ownRect.DOSizeDelta(new Vector2(ownRect.sizeDelta.x, EXPANDED_HIGHT), ExpandDuration).SetDelay(ExpandDelay).OnComplete(() => FadeCanvasGroup(true));
         }
 
@@ -46,19 +44,7 @@ namespace UISystem
             FadeCanvasGroup(false);
             ownRect.DOSizeDelta(new Vector2(ownRect.sizeDelta.x, SHRINKED_HEIGHT), ShrinkDuration).SetDelay(ShrinkDelay);
         }
-
-        private void FadeCanvasGroup(bool FadeIn)
-        {
-            if (FadeIn)
-            {
-                ActionBar.DOFade(1f, 0.2f);
-            }
-            else
-            {
-                ActionBar.DOFade(0f, 0.2f).OnComplete(() => ActionBar.gameObject.SetActive(false));
-            }
-        }
-
+        private void FadeCanvasGroup(bool FadeIn) => ActionBar.gameObject.SetActive(FadeIn);
         public void FastForwardDays(int days)
         {
             TimeStamp currStamp = TimeManager.Instance.CurrentTimeStamp;

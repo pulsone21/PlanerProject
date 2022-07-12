b1491325f7e4a7505d687816262f03f77a07d279
Johannes Böhm <pulsone21@freenet.de>
2022-07-12 14:04:45 +0200

    adding condition mechanic and rewrite priveCurve

--- Assets/_Project/VehicleSystem/DataStructure/BaseVehicle.cs
+++ Assets/_Project/VehicleSystem/DataStructure/BaseVehicle.cs
@@ -11,16 +11,17 @@ namespace VehicleSystem
     public enum VehicleType { Van, Truck, TracktorUnit };
     public abstract class BaseVehicle
     {
-        private AnimationCurve curve;
-        [SerializeField] protected string name;
-        [SerializeField, Tooltip("In tonns")] protected float capacity;
-        [SerializeField] protected bool canHandleCUBIC;
-        [SerializeField] protected bool hasForklift;
-        [SerializeField] protected bool hasCooling;
-        [SerializeField] protected bool hasCrane;
-        [SerializeField] protected int originalPrice;
-        [SerializeField] protected Image image;
-        private TimeStamp constructionYear;
+        protected AnimationCurve priceCurve;
+        protected float condition;
+        protected string name;
+        protected float capacity;
+        protected bool canHandleCUBIC;
+        protected bool hasForklift;
+        protected bool hasCooling;
+        protected bool hasCrane;
+        protected int originalPrice;
+        protected Image image;
+        protected TimeStamp constructionYear;
 
         protected BaseVehicle(BaseVehicleSO baseVehicle)
         {
@@ -33,11 +34,8 @@ namespace VehicleSystem
             this.originalPrice = baseVehicle.OriginalPrice;
             this.image = baseVehicle.Image;
             constructionYear = TimeStamp.GetRndBirthday(50, 0);
-            curve = new AnimationCurve();
-            curve.AddKey(0f, 1f);
-            curve.AddKey(0.2f, 0.5f);
-            curve.AddKey(0.35f, 0.35f);
-            curve.AddKey(1f, 0.1f);
+            priceCurve = new AnimationCurve(new Keyframe[] { new Keyframe(0f, 1f), new Keyframe(0.2f, 0.5f), new Keyframe(0.35f, 0.35f), new Keyframe(1f, 0.1f) });
+            condition = UnityEngine.Random.Range(0, 101);
         }
         public string Name => name;
         public bool CanHandleCUBIC => canHandleCUBIC;
@@ -49,10 +47,19 @@ namespace VehicleSystem
         public Image Image => image;
         public TimeStamp ConstructionYear => constructionYear;
 
+        public float Condition => condition;
+
+        public void ChangeCondition(float changeAmount)
+        {
+            condition += changeAmount;
+            if (condition > 100) condition = 100;
+            if (condition < 0) condition = 0;
+        }
+
         public float GetCalculatedPrice()
         {
             float year = constructionYear.DifferenceToNowInYears();
-            return curve.Evaluate(year);
+            return priceCurve.Evaluate(year);
         }
     }
 }

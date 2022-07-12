81da1f386b2d17812ae144f2f349ab0de6a3ab0c
Johannes Böhm <pulsone21@freenet.de>
2022-07-12 14:08:34 +0200

    implementing vehicle condition mechanic

---
+++ Assets/_Project/VehicleSystem/Controller/VehicleConditionController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EmployeeSystem;
+using TimeSystem;
+
+namespace VehicleSystem
+{
+    [RequireComponent(typeof(VehicleController))]
+    public class VehicleConditionController : MonoBehaviour
+    {
+        private VehicleController controller;
+        [SerializeField] private AnimationCurve curve;
+        private void Start() => controller = GetComponent<VehicleController>();
+        private void OnEnable() => TimeManager.Instance.RegisterForTimeUpdate(ChangeCondition, TimeManager.SubscriptionType.Day);
+        private void OnDisable() => TimeManager.Instance.UnregisterForTimeUpdate(ChangeCondition, TimeManager.SubscriptionType.Day);
+        private void ChangeCondition(TimeStamp timeStamp)
+        {
+            Employee driver = controller.Driver;
+            float change = curve.Evaluate((driver.Driving.Value / 100));
+            controller.Vehicle.ChangeCondition(change);
+            Trailer trailer = controller.Trailer;
+            if (trailer != null) trailer.ChangeCondition(change);
+        }
+    }
+}

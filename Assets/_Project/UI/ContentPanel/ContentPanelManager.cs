fe14c557f206c6b4a31e6ff058725ad18a6b6dfd
Johannes Böhm <pulsone21@freenet.de>
2022-07-27 14:16:25 +0200

    implement proper content Panel Loading

--- Assets/_Project/UI/ContentPanel/ContentPanelManager.cs
+++ Assets/_Project/UI/ContentPanel/ContentPanelManager.cs
@@ -32,7 +32,21 @@ namespace UISystem
 
         private void Start()
         {
-            if (currentPanel == null) mainPanelBtn.onClick.Invoke();
+            InitializePages();
+        }
+
+        private void InitializePages()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+            }
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+            transform.GetChild(0).gameObject.SetActive(true);
         }
 
         public static void SetPanelStatic(GameObject Panel)

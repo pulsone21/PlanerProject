00afe16a1a8e9cce5a8994ba767b2d447f5ff4cb
Johannes Böhm <pulsone21@freenet.de>
2022-08-08 14:52:12 +0200

    adding skilltraining SO

--- Assets/_Project/EmployeeSystem/DataStructures/SkillTraining.cs
+++ Assets/_Project/EmployeeSystem/DataStructures/SkillTraining.cs
@@ -7,7 +7,7 @@ namespace EmployeeSystem
     [CreateAssetMenu(fileName = "Training", menuName = "SO/EmployeeSystem/Training", order = 0)]
     public class SkillTraining : ScriptableObject
     {
-        public string Titel;
+        public string TitelID;
         public SkillName Skill;
         [Tooltip("Duration of the training in days")] public float Duration;
         public float SkillIncrease;

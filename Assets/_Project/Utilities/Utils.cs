b28f7ecf62ae75e997b85f2ebba80fb1a8700f10
Johannes Böhm <pulsone21@freenet.de>
2022-08-08 14:51:01 +0200

    new utitily function

--- Assets/_Project/Utilities/Utils.cs
+++ Assets/_Project/Utilities/Utils.cs
@@ -42,5 +42,14 @@ namespace Utilities
             textMesh.color = _fontColor;
             return textMesh;
         }
+
+        public static float GetAngleFromVectorFloat(Vector3 dir)
+        {
+            dir = dir.normalized;
+            float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            if (n < 0) n += 360;
+
+            return n;
+        }
     }
 }
